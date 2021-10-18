b641db0eed5153894cd707fe9c77d983aa3027a2
jordyf15 <jordyferdian88@gmail.com>
2021-10-18 13:59:48 +0700

    add id property for todo

--- src/scripts/todo.js
+++ src/scripts/todo.js
@@ -1,15 +1,22 @@
+import {v4 as uuidv4} from 'uuid';
 class Todo{
+    #id;
     #title;
     #description;
     #dueDate;
     #priority;
     #done;
     constructor(title, description, dueDate, priority){
+        this.#id = uuidv4();
         this.#title = title;
         this.#description = description;
         this.#dueDate = dueDate;
         this.#priority = priority;
         this.#done = false;
+    }
+
+    get id(){
+        return this.#id;
     }
 
     get title(){
