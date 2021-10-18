5bf83ea489e33d3d3cd22afe73277d24dabd20d0
jordyf15 <jordyferdian88@gmail.com>
2021-10-18 14:04:17 +0700

    create project class

---
+++ src/scripts/project.js
@@ -0,0 +1,35 @@
+import {v4 as uuidv4} from 'uuid';
+class Project{
+    #id;
+    #name;
+    #todos;
+    constructor(name){
+        this.#id = uuidv4();
+        this.#name = name;
+        this.#todos = [];
+    }
+
+    get id(){
+        return this.#id;
+    }
+
+    get name(){
+        return this.#name;
+    }
+
+    get todos(){
+        return this.#todos;
+    }
+
+    set name(name){
+        this.#name = name;
+    }
+
+    addTodo(todo){
+        this.#todos.add(todo);
+    }
+
+    removeTodo(todoId){
+        this.#todos = this.#todos.filter((todo)=> todo.id != todoId);
+    }
+}
