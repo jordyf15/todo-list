d67184dd753c1f3e7b924daa9f36b4212ea7e54e
jordyf15 <jordyferdian88@gmail.com>
2021-10-20 14:10:21 +0700

    change project and todo class attribute to public from private for stringify

--- src/scripts/project.js
+++ src/scripts/project.js
@@ -1,36 +1,20 @@
 import {v4 as uuidv4} from 'uuid';
 class Project{
-    #id;
-    #name;
-    #todos;
+    id;
+    name;
+    todos;
     constructor(name){
-        this.#id = uuidv4();
-        this.#name = name;
-        this.#todos = [];
-    }
-
-    get id(){
-        return this.#id;
-    }
-
-    get name(){
-        return this.#name;
-    }
-
-    get todos(){
-        return this.#todos;
-    }
-
-    set name(name){
-        this.#name = name;
+        this.id = uuidv4();
+        this.name = name;
+        this.todos = [];
     }
 
     addTodo(todo){
-        this.#todos.push(todo);
+        this.todos.push(todo);
     }
 
     removeTodo(todoId){
-        this.#todos = this.#todos.filter((todo)=> todo.id != todoId);
+        this.todos = this.todos.filter((todo)=> todo.id != todoId);
     }
 }
 
--- src/scripts/todo.js
+++ src/scripts/todo.js
@@ -1,62 +1,18 @@
 import {v4 as uuidv4} from 'uuid';
 class Todo{
-    #id;
-    #title;
-    #description;
-    #dueDate;
-    #priority;
-    #done;
+    id;
+    title;
+    description;
+    dueDate;
+    priority;
+    done;
     constructor(title, description, dueDate, priority){
-        this.#id = uuidv4();
-        this.#title = title;
-        this.#description = description;
-        this.#dueDate = dueDate;
-        this.#priority = priority;
-        this.#done = false;
-    }
-
-    get id(){
-        return this.#id;
-    }
-
-    get title(){
-        return this.#title;
-    }
-
-    get description(){
-        return this.#description;
-    }
-
-    get dueDate(){
-        return this.#dueDate;
-    }
-
-    get priority() {
-        return this.#priority;
-    }
-
-    get done(){
-        return this.#done;
-    }
-
-    set title(title){
-        this.#title = title;
-    }
-
-    set description(description){
-        this.#description = description;
-    }
-
-    set dueDate(dueDate){
-        this.#dueDate = dueDate;
-    }
-
-    set priority(priority){
-        this.#priority = priority;
-    }
-
-    set done(done){
-        this.#done = done;
+        this.id = uuidv4();
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+        this.done = false;
     }
 }
 
