8ce1dbdf131a64f14fbe44907770e9add714d90d
jordyf15 <jordyferdian88@gmail.com>
2021-10-20 14:15:27 +0700

    delete unnecessary console log

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -3,22 +3,15 @@ import {Project} from './project';
 const app = (function(){
     let storage = [];
     if(localStorage.getItem("todo-list")){
-        console.log(localStorage.getItem("todo-list"))
         storage = JSON.parse(localStorage.getItem("todo-list"));
     }else{
         setDefaultStorage();
-        console.log(storage);
-        console.log(JSON.stringify(storage));
-        // localStorage.setItem('todo-list', JSON.stringify(storage));
+        localStorage.setItem('todo-list', JSON.stringify(storage));
     }
 
     function setDefaultStorage(){
         const defaultProject = new Project('Default');
-        const test = new Project('asd');
-        console.log(defaultProject);
-        console.log(JSON.stringify(defaultProject));
         storage.push(defaultProject);
-        storage.push(test);
     }
 
     function getStorage(){
